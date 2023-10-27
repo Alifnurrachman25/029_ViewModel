cd80ba7e8ce60f612f4085e24b59e74a925a624d
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-10-27 09:37:31 +0700

    menambahkan icon back, register dan create your account

--- app/src/main/java/com/example/kotlinio/MainActivity.kt
+++ app/src/main/java/com/example/kotlinio/MainActivity.kt
@@ -4,7 +4,9 @@ import CobaViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -34,8 +36,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -96,6 +101,33 @@ fun TampilForm(cobaViewModel: CobaViewModel = viewModel()){
     val dataForm : DataForm
     val uiState by cobaViewModel.uiState.collectAsState()
     dataForm = uiState
+
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.arrow_back),
+                contentDescription = null,
+                modifier = Modifier)
+            Text(text = "Register",
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+    Text(text = "Create Your Account",
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp)
+        )
 
     OutlinedTextField(
         value = textNama,
